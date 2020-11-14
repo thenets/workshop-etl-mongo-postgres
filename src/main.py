7d9f8be6764d16bf6925e018a158ab64aa7a9074
Luiz Costa <phelip@thenets.org>
2020-11-13 23:19:50 -0300

    Populate mongo if hasn't 'pokedex' database

--- src/main.py
+++ src/main.py
@@ -3,20 +3,41 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
+import requests
+import json
 
 # %% Import data from DocumentDB
 from pymongo import MongoClient
 if 'MONGO_URL' in os.environ:
-    mongo_client = MongoClient(os.environ['MONGO_URL'])
+    mongo = MongoClient(os.environ['MONGO_URL'])
 else:
-    mongo_client = MongoClient()
+    mongo = MongoClient()
 
 # %% Populate database if not exist
-database_names = [x['name'] for x in mongo_client.list_databases()]
+def pokeapi_get_pokemon_by_id(pokemon_id: int):
+    url = f"https://pokeapi-cache.herokuapp.com/api/v2/pokemon/{pokemon_id}"
+    r = requests.get(url)
+    return json.loads(r.content)
+
+def mongo_insert_pokemon(pokemon: dict):
+    m_pokedex = mongo.get_database('pokedex')
+    m_pokedex_pokemon = m_pokedex.get_collection('pokemon')
+    m_pokedex_pokemon.insert_one(pokemon)
+
+def mongo_get_pokemon_by_name(pokemon_name: str):
+    m_pokedex = mongo.get_database('pokedex')
+    m_pokedex_pokemon = m_pokedex.get_collection('pokemon')
+    return m_pokedex_pokemon.find_one({"name": pokemon_name})
+
+database_names = [x['name'] for x in mongo.list_databases()]
 
 if 'pokedex' not in database_names:
-    mongo_client.data
-database_names
+    for i in range(10):
+        mongo_insert_pokemon(
+            pokeapi_get_pokemon_by_id(i+1)
+        )
+
+# %%
 
 # %%
 import pandas as pd
