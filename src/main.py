82bac55ba894b7ae3d7fd3b1d52943d4db049317
Luiz Costa <phelip@thenets.org>
2020-11-14 00:22:54 -0300

    Finished async requests to PokeAPI

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import os
 import requests
 import json
 
-# %% Import data from DocumentDB
+# %% Get Mongo client
 from pymongo import MongoClient
 if 'MONGO_URL' in os.environ:
     mongo = MongoClient(os.environ['MONGO_URL'])
@@ -19,11 +19,12 @@ def pokeapi_get_pokemon_by_id(pokemon_id: int):
     r = requests.get(url)
     return json.loads(r.content)
 
-def mongo_insert_pokemon(pokemon: dict):
+def mongo_insert_pokemon(pokemon: dict, log_stdout=True):
     m_pokedex = mongo.get_database('pokedex')
     m_pokedex_pokemon = m_pokedex.get_collection('pokemon')
     m_pokedex_pokemon.insert_one(pokemon)
-    print(f"[INSERTED] {pokemon['name']}")
+    if log_stdout:
+        print(f"[INSERTED] {pokemon['name']}")
 
 def mongo_insert_pokemon_by_id(pokemon_id: int):
     pokemon = pokeapi_get_pokemon_by_id(pokemon_id)
@@ -36,36 +37,43 @@ def mongo_get_pokemon_by_name(pokemon_name: str):
 
 database_names = [x['name'] for x in mongo.list_databases()]
 
-
-# %%
 if 'pokedex' not in database_names:
-    populate_async = True
+    populate_async = True # Param
     pokedex_up_to = 220
 
     if populate_async:
         from multiprocessing import Pool, TimeoutError
         import time
         import os
+        print(f"[PokeAPI] Retrieving data of {pokedex_up_to} Pokémon.")
         with Pool(processes=10) as pool:
-            pool.map_async(
-                mongo_insert_pokemon_by_id, range(pokedex_up_to+1)[1:]
-            ).get(timeout=60)
+            print("          downloading... (timeout of 120s)")
+            pokemon_list = pool.map_async(
+                pokeapi_get_pokemon_by_id, range(pokedex_up_to+1)[1:]
+            ).get(timeout=120)
+            for pokemon in pokemon_list:
+                mongo_insert_pokemon(pokemon, False)
+            print("          download complete!")
     else:
         for i in range(pokedex_up_to+1)[1:]:
             mongo_insert_pokemon(
                 pokeapi_get_pokemon_by_id(i)
             )
 
-# %%
-out
+# %% Get PostgreSQL client
+# from sqlalchemy import event
+# from sqlalchemy.schema import CreateSchema
 
-# %%
-import pandas as pd
-
-jobs_df = pd.read_csv('data/nyc-jobs.csv')
-
-print("OI")
+# event.listen(Base.metadata, 'before_create', CreateSchema('my_schema'))
 
 
-# %%
-from sqlalchemy.types import Integer, Text, String, DateTime
+# # %%
+# import pandas as pd
+
+# jobs_df = pd.read_csv('data/nyc-jobs.csv')
+
+# print("OI")
+
+
+# # %%
+# from sqlalchemy.types import Integer, Text, String, DateTime
